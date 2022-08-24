d69ef05dd554ee848b80482bd5642c37f35d4212
Mateus Galvão <mateustgalvao@outlook.com>
2022-08-23 23:28:43 -0300

    submissao exercicios bonus 4.3

--- fundamentos/bloco-04/4.3/exercicios-bonus.js
+++ fundamentos/bloco-04/4.3/exercicios-bonus.js
@@ -1,10 +1,63 @@
-function quadrado() {
-    const n = 5
+// Gerar linha com conteúdo alinhado à esquerda
+function createLine(n) {
+    let lineArray = []
 
-    for (let i = 0; i < n; i++) {
-        for (let j = 0; j < n; j++) {
-            console.log('*')
+    for (let i = 0; i < n; i += 1) {
+        lineArray.push('*')
+    }
+    return lineArray.join('')
+}
+
+// Gerar linha com conteúdo alinhado à direita
+function createLineRightAligned(n) {
+    let lineArray = []
+
+    for (let i = 1; i <= n; i += 1) {
+        if (i < n) {
+            lineArray.push(' ')
+        } else {
+            lineArray.push('*')
         }
     }
+    return lineArray.join('')
 }
-quadrado()
+
+// Gerar linha com conteúdo centralizado
+function createCenteredLine(n) {
+    let lineArray = []
+
+    for (let i = 1; i <= n; i += 1) {
+        // ENCONTRAR O PADRÃO!!
+    }
+    return lineArray.join('')
+}
+console.log(createCenteredLine(5))
+
+// Fazer quadrado
+function square(n) {
+
+    for (let i = 0; i < n; i += 1) {
+        console.log(createLine(n))
+    }
+}
+
+// Fazer triangulo retângulo
+function rightTriangle(n) {
+
+    for (let i = 1; i <= n; i += 1) {
+        console.log(createLine(i))
+    }
+}
+
+// Fazer triângulo retângulo virado para o outro lado
+function rightTriangleRightAligned(n) {
+
+    for (let i = 0; i < n; i += 1) {
+        console.log(createLineRightAligned(n - i) + createLine(i))
+    }
+}
+
+// Pirâmide de base n
+function piramide(n) {
+
+}
