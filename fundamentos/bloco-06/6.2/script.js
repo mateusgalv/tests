eaf72d693c9eb2a2a547f7792bb11d88cc65ddd1
Mateus Galvão <110851306+mateusgalv@users.noreply.github.com>
2022-09-26 16:25:23 -0300

    Delete fundamentos directory

--- fundamentos/bloco-06/6.2/script.js
+++
@@ -1,7 +0,0 @@
-const modalBtn = document.getElementById('modal-btn')
-const myModal = new bootstrap.Modal(document.getElementById('myModal'))
-
-modalBtn.addEventListener('click', (e) => {
-	e.preventDefault()
-	myModal.show()
-})
