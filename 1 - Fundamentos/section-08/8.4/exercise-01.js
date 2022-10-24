a784dade46551fdbfa0b0faab1827b395b59443b
Mateus Galvão <mateustgalvao@outlook.com>
2022-10-24 20:11:03 -0300

    adiciona exercícios da section03

--- 1 - Fundamentos/section-08/8.4/exercise-01.js
+++
@@ -1,9 +0,0 @@
-// Transformar matriz em um array
-const arrays = [
-	['1', '2', '3'],
-	[true],
-	[4, 5, 6],
-];
-
-const mergedArray = arrays.reduce((acc, array) => acc.concat(array))
-console.log(mergedArray)
